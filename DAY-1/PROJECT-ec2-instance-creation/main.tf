252a4fcb4d8b08b77d665f7f147ab3e0e8ae5612
Appi Reddy <appireddy.777@outlook.com>
2023-10-01 21:47:29 +0530

    terraform-code1

--- DAY-1/PROJECT-ec2-instance-creation/main.tf
+++ DAY-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
     ami = "ami-03265a0778a880afb"
     instance_type = "t2.micro"
     subnet_id = "subnet-0877957c3cfcaaa96"
-    key_name = "aws-login"
+    key_name = "aws-login-1"
   
 }
