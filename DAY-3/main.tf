f5f950cfb56c596279a67fcf474b30b7b3cba746
Appi Reddy <appireddy.777@outlook.com>
2023-10-21 23:00:21 +0530

    abv-modules

--- DAY-3/main.tf
+++ DAY-3/main.tf
@@ -6,5 +6,5 @@ module "ec2-instance" {
   source = "./modules/ec2-instance"
   ami_value = "ami-0df435f331839b2d6"
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-0ec1894b05ddfda4-4"
+  subnet_id_value = "subnet-0ec1894b05ddfda44"
 }
