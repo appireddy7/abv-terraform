0be12f3686311c0aac43966a2a2ef405ab81965c
Appi Reddy <appireddy.777@outlook.com>
2023-10-21 12:50:01 +0530

    abv-modules

---
+++ DAY-3/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2-instance" {
+  source = "./modules/ec2-instance"
+  ami_value = "ami-0df435f331839b2d6"
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-0ec1894b05ddfda44"
+}
